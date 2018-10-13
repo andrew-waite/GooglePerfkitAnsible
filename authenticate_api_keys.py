96586f4d9878896e993bbb38ffc46744de3b647b
Andrew Waite <andrew@Andrews-MacBook-Pro.local>
2018-10-13 22:44:53 +1100

    Fixed authenitcate script

--- authenticate_api_keys.py
+++ authenticate_api_keys.py
@@ -8,67 +8,65 @@ import sys
 
 class api_key_reader():
 
-	def __init__(self):
-		self.config = self.import_yaml()
+        def __init__(self):
+                self.config = self.import_yaml()
 
-	def import_yaml(self):
-		my_path = os.path.abspath(os.path.dirname(__file__))
-		path = os.path.join(my_path, 'passwords/keys.yml')
-		print(path)
-		with open(path, 'r') as stream:
-			try:
-				return yaml.load(stream)
-			except yaml.YAMLError as exc:
-				print(exc)
+        def import_yaml(self):
+                my_path = os.path.abspath(os.path.dirname(__file__))
+                path = os.path.join(my_path, 'passwords/keys.yml')
+                print(path)
+                with open(path, 'r') as stream:
+                        try:
+                                return yaml.load(stream)
+                        except yaml.YAMLError as exc:
+                                print(exc)
 
-	def runAuthCommands(self):
-		#Digital Ocean configure
-		try:
-			if self.config['digital_ocean_key'] is None: # The variable
-				print('The key is empty skipping')
-		except NameError:
-			print ("The digital ocean key is not set. Skipping this step...")
-		else:
-			command = 'doctl auth init -t ' + self.config['digital_ocean_key']
-			result = subprocess.call(command, shell=True)
+        def runAuthCommands(self):
+                #Digital Ocean configure
+                try:
+                        if self.config['digital_ocean_key'] is None: # The variable
+                                print('The key is empty skipping')
+                        else:
+                                command = 'doctl auth init -t ' + self.config['digital_ocean_key']
+                                result = subprocess.call(command, shell=True)
+                except NameError:
+                        print ("There was an error in getting the keys for digital ocean..")
 
-		#AWS configure
-		try:
-			if self.config['aws_keys']['aws_access_key_id'] is None: # The variable
-				print('The key is empty skipping')
-		except NameError:
-			print ("The aws key is not set. Skipping this step...")
-		else:
-			command_one = 'aws configure set aws_access_key_id ' + self.config['aws_keys']['aws_access_key_id']
-			command_two = 'aws configure set aws_secret_access_key ' + self.config['aws_keys']['aws_secret_access_key']
-			command_three = 'aws configure set default.region ' + self.config['aws_keys']['default_region']
+                #AWS configure
+                try:
+                        if self.config['aws_keys']['aws_access_key_id'] is None: # The variable
+                                print('The key is empty skipping')
+                        else:
+                                command_one = 'aws configure set aws_access_key_id ' + self.config['aws_keys']['aws_access_key_id']
+                                command_two = 'aws configure set aws_secret_access_key ' + self.config['aws_keys']['aws_secret_access_key']
+                                command_three = 'aws configure set default.region ' + self.config['aws_keys']['default_region']
+                                subprocess.call(command_one, shell=True)
+                                subprocess.call(command_two, shell=True)
+                                subprocess.call(command_three, shell=True)
+                except NameError:
+                        print ("There was an error in getting the keys for aws.")
 
-			subprocess.call(command_one, shell=True)
-			subprocess.call(command_two, shell=True)
-			subprocess.call(command_three, shell=True)
+                #Azure configure
+                try:
+                        if self.config['azure_keys']['username'] is None: # The variable
+                                print('The key is empty skipping')
+                        else:
+                                command = 'azure login -u ' + self.config['azure_keys']['username'] + ' -p ' + self.config['azure_keys']['password']
+                                subprocess.call(command, shell=True)
+                                subprocess.call('azure config mode arm', shell=True)
+                                subprocess.call('azure provider register Microsoft.Compute', shell=True)
+                                subprocess.call('azure provider register Microsoft.Network', shell=True)
+                except NameError:
+                        print ("There was an error in getting the keys for azure.")
 
-		#Azure configure
-		try:
-			if self.config['azure_keys']['username'] is None: # The variable
-				print('The key is empty skipping')
-		except NameError:
-			print ("The Azure username is not set. Skipping this step...")
-		else:
-			command = 'azure login -u ' + self.config['azure_keys']['username'] + ' -p ' + self.config['azure_keys']['password']
-			subprocess.call(command, shell=True)
-			subprocess.call('azure config mode arm', shell=True)
-			subprocess.call('azure provider register Microsoft.Compute', shell=True)
-			subprocess.call('azure provider register Microsoft.Network', shell=True)
-
-		#Openstack configure
-		try:
-			if self.config['openstack'] is None: # The variable
-				print('The key is empty skipping')
-		except NameError:
-			print ("The openstack path has not been set. Skipping this step...")
-		else:
-			command = 'source ' + self.config['openstack']
-			subprocess.call(command, shell=True)
+                #Openstack configure
+                try:
+                        if self.config['openstack'] is None: # The variable
+                                print('The key is empty skipping')
+                        else:
+                                command = 'source ' + self.config['openstack']
+                except NameError:
+                        print ("There was an error in getting the keys for openstack")
 
 	def runBenchmarks(self):
 		if self.config['digital_ocean_key'] is not None:
@@ -85,7 +83,8 @@ class api_key_reader():
 			subprocess.call(command, shell=True)
 
 def main():
-	perfkitRun = api_key_reader()
-	perfkitRun.runAuthCommands()
+        perfkitRun = api_key_reader()
+        perfkitRun.runAuthCommands()
+		perfkitRun.runBenchmarks()
 
 main()
